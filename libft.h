4f10dfedf56ec2f247964367b9396c1ca8808aed
Khadijah Taherah <59830938+khadijahtaherah@users.noreply.github.com>
2024-06-18 21:06:48 +0800

    Updated

--- libft.h
+++ libft.h
@@ -31,10 +31,17 @@ int		ft_strlen(const char *s);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_atoi(const char *str);
+void	bzero(void *s, size_t n);
 void	*ft_memset(void *b, int c, int len);
 void	*ft_memcpy(void *dest, void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int     ft_strlen(const char *s);
+int	    ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
