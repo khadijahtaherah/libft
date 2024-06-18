4f12cf6cfd5030ff99798874f91cb66c39f0bc76
Khadijah Taherah <59830938+khadijahtaherah@users.noreply.github.com>
2024-06-18 14:15:48 +0800

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -2,17 +2,18 @@
 #include "libft.h"
 
 /* @function  ft_calloc
- * @params    nmemb: no of elements, size: size
+ * @params    count: no of elements to allocate, size: size of each element
  * @brief     Allocates  memory for an array of nmemb elements of size bytes each 
  * @return    Returns a pointer to the allocated memory. The memory is set to zero.
               If nmemb or size is 0, returns either NULL, or a unique pointer value 
               that can later be successfully passed	to free().
  */
-void  *ft_calloc(size_t nmemb, size_t size)
+void  *ft_calloc(size_t count, size_t size)
 {
   void  *ptr;
 
-  ptr = malloc(sizeof(size) * nmemb);
-  ft_memset(ptr, 0, sizeof(size) * nmemb);
+  if (!(ptr = malloc(count * size)))
+    return (NULL);
+  ft_memset(ptr, 0, count * size);
   return (ptr);
 }
