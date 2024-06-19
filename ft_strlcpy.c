fd8e5322ad6cb70a5586ed9a47d9f3b463e3ac7c
Khadijah Taherah <59830938+khadijahtaherah@users.noreply.github.com>
2024-06-19 19:17:04 +0800

    Adjust test case

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -17,7 +17,7 @@
  * @brief             
  * @return      
  */
-
+/*
 int	ft_strlen(const char *s)
 {
 	int	i;
@@ -26,7 +26,7 @@ int	ft_strlen(const char *s)
 	while (s[i])
 		i++;
 	return (i);
-}
+}*/
 
 size_t  ft_strlcpy(char *dest, const char *src, size_t dstsize)
 {
@@ -48,13 +48,31 @@ size_t  ft_strlcpy(char *dest, const char *src, size_t dstsize)
     *dest = '\0';
     return (src_len);
 }
-
+/*
 #include <stdio.h>
 #include <string.h>
 
 int main(void)
 {
- 
+    char src[] = "Hello, world!";
+    char dest[20];  // Destination buffer
+
+    // Test case 1: Normal operation
+    size_t result = ft_strlcpy(dest, src, sizeof(dest));
+    printf("Copied string: %s\n", dest);
+    printf("Return value: %zu\n", result);  // Should be length of src
+
+    // Test case 2: Buffer is just big enough
+    char small_dest[14];  // Size is exactly strlen(src) + 1
+    size_t result_small = ft_strlcpy(small_dest, src, sizeof(small_dest));
+    printf("Copied string (small buffer): %s\n", small_dest);
+    printf("Return value (small buffer): %zu\n", result_small);  // Should be length of src
+
+    // Test case 3: Destination buffer is too small
+    char tiny_dest[5];  // Size is smaller than strlen(src) + 1
+    size_t result_tiny = ft_strlcpy(tiny_dest, src, sizeof(tiny_dest));
+    printf("Copied string (tiny buffer): %s\n", tiny_dest);
+    printf("Return value (tiny buffer): %zu\n", result_tiny);  // Should be length of src
 
     return 0;
-}
+}*/
