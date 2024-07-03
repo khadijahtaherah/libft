3d44e920ef9ca8ad684bd2f7000d76a1be53a0c1
Khadijah Taherah <59830938+khadijahtaherah@users.noreply.github.com>
2024-07-03 17:06:32 +1000

    ft_trim. Must check for test

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,18 +12,52 @@
 
 #include "libft.h"
 
+int	isset(char const *set, int c)
+{
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
 /* @function	ft_strtrim - trim set characters from main string
  * @params		s1: string to be trimmed, set: set of characters to trim
  * @brief		Allocates with malloc. Trim s1 with the characters specified in
  * 				'set' at the beginning and at the end of s1
  * @return		Trimmed copy of s1. NULL if memory allocation fails
  */
-char	*ft_strtrim(char const *s1, char const *set)
+char    *ft_strtrim(char const *s1, char const *set)
 {
-	char	*trim;
-	
-	trim = malloc((ft_strlen(s1) + ft_strlen(set) + 1) * sizeof(char));
-	if (!trim)
-		return (NULL);
+    char    *trim;
+    int        i;
+    
+    trim = malloc((strlen(s1) + 1) * sizeof(char));
+    if (!trim)
+        return (NULL);
+    strcpy(trim, s1);
+    while (isset(set, *trim))
+        trim++;
+    i = strlen(trim) - 1;
+    trim = trim + i;
+    while (isset(set, *trim))
+    {
+        trim--;
+        i--;
+    }
+    trim++;
+    *trim = '\0';
+    trim = trim - i - 1;
+    return (trim);
 }
-
+/*
+int main()
+{
+    char const *s1 = "aaababHello Abyss aabb";
+    char const *set = "ab";
+    char *res = ft_strtrim(s1, set);
+    printf("%s\n", res);
+    return (0);
+}*/
