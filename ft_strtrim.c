757758250bcd22b1979c505064ac897af4c279e6
Khadijah Taherah <59830938+khadijahtaherah@users.noreply.github.com>
2024-07-09 21:34:52 +1000

    Fixed. Passed all test and norminette

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,6 +12,41 @@
 
 #include "libft.h"
 
+/* @function	ft_strtrim - trim set characters from main string
+ * @params		s1: string to be trimmed, set: set of characters to trim
+ * @brief		Allocates with malloc. Trim s1 with the characters specified in
+ * 				'set' at the beginning and at the end of s1
+ * @return		Trimmed copy of s1. NULL if memory allocation fails
+ */
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*source;
+	char	*trim;
+	int		i;
+	int		j;
+	int		k;
+
+	source = malloc((ft_strlen(s1) + 1) * sizeof(char));
+	if (!source)
+		return (NULL);
+	ft_strlcpy(source, s1, ft_strlen(s1) + 1);
+	i = 0;
+	while (isset(set, source[i]))
+		i++;
+	j = ft_strlen(source) - 1;
+	while (j >= i && isset(set, source[j]))
+		j--;
+	trim = malloc((j - i + 2) * sizeof(char));
+	if (!trim)
+		return (NULL);
+	k = 0;
+	while (i <= j)
+		trim[k++] = source[i++];
+	trim[k] = '\0';
+	free(source);
+	return (trim);
+}
+
 int	isset(char const *set, int c)
 {
 	while (*set)
@@ -21,37 +56,6 @@ int	isset(char const *set, int c)
 		set++;
 	}
 	return (0);
-}
-
-/* @function	ft_strtrim - trim set characters from main string
- * @params		s1: string to be trimmed, set: set of characters to trim
- * @brief		Allocates with malloc. Trim s1 with the characters specified in
- * 				'set' at the beginning and at the end of s1
- * @return		Trimmed copy of s1. NULL if memory allocation fails
- */
-char    *ft_strtrim(char const *s1, char const *set)
-{
-    char    *trim;
-    char    *ptr;
-    int        i;
-    
-    trim = malloc((strlen(s1) + 1) * sizeof(char));
-    if (!trim)
-        return (NULL);
-    strcpy(trim, s1);
-    while (isset(set, *trim))
-        trim++;
-    ptr = trim;
-    i = strlen(trim) - 1;
-    trim = trim + i;
-    while (isset(set, *trim))
-    {
-        trim--;
-        i--;
-    }
-    trim++;
-    *trim = '\0';
-    return (ptr);
 }
 /*
 int main()
