17c17ca2a37df14147c96c0904ec90120c1b7a91
Khadijah Taherah <59830938+khadijahtaherah@users.noreply.github.com>
2024-07-15 02:32:47 +1000

    intial commit

--- ft_itoa.c
+++ ft_itoa.c
@@ -11,3 +11,68 @@
 /* ************************************************************************** */
 
 
+/* @function	ft_itoa - Convert integer to string
+   @params		n: the integer to convert
+   @brief		Allocates with malloc. Returns a string representing the int.
+				Negative numbers must be handled.
+   @return		Returns the string representing n. 
+				NULL if allocation fails.
+ */
+char	*ft_itoa(int n)
+{
+	char	*string;
+	int 	len;
+
+	len = count_digit(n);
+	string = malloc ((len + 1) * sizeof(char));
+	if (!string)
+		return (NULL);
+	ft_putnbr(string, n);
+	return (string);
+}
+
+int		count_digit(int n)
+{
+	int	count;
+
+	count = 0;
+	if (n < 0)
+		count++;
+	while (n > 0)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_putnbr(char *s, int n)
+{
+	char	*num;
+	long	nbl;
+
+	num = "0123456789";
+	nbl = (long)n;
+	if (nbl < 0)
+	{
+		*s = '-';
+		s++;
+		nbl = -nbl;
+	}
+	if (nbl > 9)
+		ft_putnbr(s, nbl / 10);
+	*s++ = (nbl % 10) + '0';
+	*s = '\0';
+	return (s);
+}
+
+int	main(void)
+{
+	char	*s;
+
+	s = ft_itoa(-214);
+	printf("%s\n", s);
+	//s = ft_itoa(214);
+	//printf("%s\n", s);
+	return (0);
+}
