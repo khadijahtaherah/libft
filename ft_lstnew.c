19397d29339652f49cc1309b6f275232d8ebb8ba
Khadijah Taherah <59830938+khadijahtaherah@users.noreply.github.com>
2024-07-16 15:57:42 +1000

    updated

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,9 +1,11 @@
 #include "libft.h"
 
 /* @function	ft_lstnew
-   @params		content: the content to be assigned
-   @brief
-   @return
+   @params		content: the content to be assigned to 
+   @brief		Allocates (with malloc(3)). The member variable ’content’
+   				is initialized with the value of the parameter ’content’.
+				The variable ’next’ is initialized to NULL.
+   @return		The new struct node
 */
 
 t_list *ft_lstnew(void *content)
